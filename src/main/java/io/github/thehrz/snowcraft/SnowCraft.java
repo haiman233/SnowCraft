476dd822a596e1cb7084743059f768f519e03aad
Thehrz <thehrz@outlook.com>
2021-02-09 10:58:15 +0800

    Fix OriginalAutomatedCraftingChamber Add config.yml

--- src/main/java/io/github/thehrz/snowcraft/SnowCraft.java
+++ src/main/java/io/github/thehrz/snowcraft/SnowCraft.java
@@ -3,11 +3,15 @@ package io.github.thehrz.snowcraft;
 import io.github.thehrz.snowcraft.object.machines.OriginalAutomatedCraftingChamber;
 import io.github.thehrz.snowcraft.setup.ItemsSetup;
 import io.izzel.taboolib.loader.Plugin;
+import io.izzel.taboolib.module.config.TConfig;
+import io.izzel.taboolib.module.config.TConfigWatcher;
+import io.izzel.taboolib.module.inject.TInject;
 import me.mrCookieSlime.CSCoreLibPlugin.general.Inventory.Item.CustomItemSerializer;
 import org.bukkit.Bukkit;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.Recipe;
 import org.bukkit.inventory.ShapedRecipe;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 import java.util.Iterator;
@@ -17,6 +21,15 @@ import java.util.Map;
  * @author Thehrz
  */
 public final class SnowCraft extends Plugin {
+    @NotNull
+    private static final TConfigWatcher WATCHER = new TConfigWatcher();
+    @TInject("config.yml")
+    public static TConfig conf;
+
+    @NotNull
+    public static TConfig getConf() {
+        return conf;
+    }
 
     @Override
     public void onDisable() {
@@ -25,35 +38,32 @@ public final class SnowCraft extends Plugin {
 
     @Override
     public void onEnable() {
-        Iterator<Recipe> recipeIterator = Bukkit.getServer().recipeIterator();
+        OriginalAutomatedCraftingChamber.updateBlackList();
+        conf.listener(() -> {
+            OriginalAutomatedCraftingChamber.updateBlackList();
+            System.out.println("加载成功");
+        });
+        iteratorRecipes();
+        ItemsSetup.setupItems();
+    }
 
+    private void iteratorRecipes() {
+        Iterator<Recipe> recipeIterator = Bukkit.getServer().recipeIterator();
         while (recipeIterator.hasNext()) {
             StringBuilder builder = new StringBuilder();
             Recipe recipe = recipeIterator.next();
             if (recipe instanceof ShapedRecipe) {
                 ShapedRecipe shapedRecipe = (ShapedRecipe) recipe;
                 Map<Character, ItemStack> ingredientMap = shapedRecipe.getIngredientMap();
-
                 Iterator<String> iterator = Arrays.stream(shapedRecipe.getShape()).iterator();
-                int i = 0;
                 while (iterator.hasNext()) {
                     for (char c : iterator.next().toCharArray()) {
-                        if (i > 0) {
-                            builder.append(" </slot> ");
-                        }
-                        if (ingredientMap.get(c) == null) {
-                            continue;
-                        }
+                        builder.append(" </slot> ");
                         builder.append(CustomItemSerializer.serialize(ingredientMap.get(c), CustomItemSerializer.ItemFlag.DATA, CustomItemSerializer.ItemFlag.ITEMMETA_DISPLAY_NAME, CustomItemSerializer.ItemFlag.ITEMMETA_LORE, CustomItemSerializer.ItemFlag.MATERIAL));
-
-                        i++;
                     }
                 }
-
                 OriginalAutomatedCraftingChamber.recipes.put(builder.toString(), shapedRecipe.getResult());
             }
         }
-
-        ItemsSetup.setupItems();
     }
 }
--- src/main/java/io/github/thehrz/snowcraft/object/machines/OriginalAutomatedCraftingChamber.java
+++ src/main/java/io/github/thehrz/snowcraft/object/machines/OriginalAutomatedCraftingChamber.java
@@ -1,5 +1,6 @@
 package io.github.thehrz.snowcraft.object.machines;
 
+import io.github.thehrz.snowcraft.SnowCraft;
 import me.mrCookieSlime.CSCoreLibPlugin.general.Inventory.InvUtils;
 import me.mrCookieSlime.CSCoreLibPlugin.general.Inventory.Item.CustomItemSerializer;
 import me.mrCookieSlime.Slimefun.Lists.RecipeType;
@@ -8,10 +9,13 @@ import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.machines.AutomatedCrafting
 import me.mrCookieSlime.Slimefun.api.BlockStorage;
 import me.mrCookieSlime.Slimefun.api.energy.ChargableBlock;
 import me.mrCookieSlime.Slimefun.api.inventory.BlockMenu;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -19,6 +23,7 @@ import java.util.Map;
  */
 public class OriginalAutomatedCraftingChamber extends AutomatedCraftingChamber {
     public static Map<String, ItemStack> recipes = new HashMap<>();
+    public static List<ItemStack> blacklist = new ArrayList<>();
 
     public OriginalAutomatedCraftingChamber(Category category, ItemStack item, String name, RecipeType recipeType, ItemStack[] recipe) {
         super(category, item, name, recipeType, recipe);
@@ -29,6 +34,16 @@ public class OriginalAutomatedCraftingChamber extends AutomatedCraftingChamber {
         return 32;
     }
 
+    public static void updateBlackList() {
+        blacklist.clear();
+        SnowCraft.getConf().get("OriginalAutomatedCraftingChamber.blacklist");
+        for (String blacklist : SnowCraft.getConf().getStringList("OriginalAutomatedCraftingChamber.blacklist")) {
+            if (Material.getMaterial(blacklist) != null) {
+                OriginalAutomatedCraftingChamber.blacklist.add(new ItemStack(Material.getMaterial(blacklist)));
+            }
+        }
+    }
+
     @Override
     protected void tick(Block b) {
         if ("false".equals(BlockStorage.getLocationInfo(b.getLocation(), "enabled")) || ChargableBlock.getCharge(b) < getEnergyConsumption()) {
@@ -36,30 +51,36 @@ public class OriginalAutomatedCraftingChamber extends AutomatedCraftingChamber {
         }
         BlockMenu menu = BlockStorage.getInventory(b);
         StringBuilder builder = new StringBuilder();
-        int i = 0;
-        for (int j = 0; j < 9; j++) {
-            if (i > 0) {
-                builder.append(" </slot> ");
+        for (int x = 1, m = 0; x <= 3; x++) {
+            int n = 0;
+            StringBuilder row = new StringBuilder();
+            for (int y = 1; y <= 3; y++, m++) {
+                ItemStack item = menu.getItemInSlot(getInputSlots()[m]);
+                if (item != null && item.getAmount() == 1) {
+                    return;
+                }
+                if (item == null) {
+                    n++;
+                }
+                row.append(" </slot> ");
+                row.append(CustomItemSerializer.serialize(item, CustomItemSerializer.ItemFlag.DATA, CustomItemSerializer.ItemFlag.ITEMMETA_DISPLAY_NAME, CustomItemSerializer.ItemFlag.ITEMMETA_LORE, CustomItemSerializer.ItemFlag.MATERIAL));
             }
-            ItemStack item = menu.getItemInSlot(getInputSlots()[j]);
-            if (item != null && item.getAmount() == 1) {
-                return;
+            if (n != 3) {
+                builder.append(row);
             }
-            if (item == null) {
-                continue;
-            }
-            builder.append(CustomItemSerializer.serialize(item, CustomItemSerializer.ItemFlag.DATA, CustomItemSerializer.ItemFlag.ITEMMETA_DISPLAY_NAME, CustomItemSerializer.ItemFlag.ITEMMETA_LORE, CustomItemSerializer.ItemFlag.MATERIAL));
-            i++;
         }
+
         String input = builder.toString();
         if (OriginalAutomatedCraftingChamber.recipes.containsKey(input)) {
             final ItemStack output = OriginalAutomatedCraftingChamber.recipes.get(input).clone();
-            if (this.fits(b, new ItemStack[]{output})) {
-                this.pushItems(b, new ItemStack[]{output});
-                ChargableBlock.addCharge(b, -this.getEnergyConsumption());
-                for (int k = 0; k < 9; ++k) {
-                    if (menu.getItemInSlot(this.getInputSlots()[k]) != null) {
-                        menu.replaceExistingItem(this.getInputSlots()[k], InvUtils.decreaseItem(menu.getItemInSlot(this.getInputSlots()[k]), 1));
+            if (!blacklist.contains(output)) {
+                if (this.fits(b, new ItemStack[]{output})) {
+                    this.pushItems(b, new ItemStack[]{output});
+                    ChargableBlock.addCharge(b, -this.getEnergyConsumption());
+                    for (int k = 0; k < 9; ++k) {
+                        if (menu.getItemInSlot(this.getInputSlots()[k]) != null) {
+                            menu.replaceExistingItem(this.getInputSlots()[k], InvUtils.decreaseItem(menu.getItemInSlot(this.getInputSlots()[k]), 1));
+                        }
                     }
                 }
             }
