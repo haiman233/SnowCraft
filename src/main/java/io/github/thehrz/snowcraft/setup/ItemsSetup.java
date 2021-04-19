0124a580e31adba56089ef25fdcce8f06b051cc5
Thehrz <thehrz@outlook.com>
2021-04-19 12:31:11 +0800

    Add Lantern

--- src/main/java/io/github/thehrz/snowcraft/setup/ItemsSetup.java
+++ src/main/java/io/github/thehrz/snowcraft/setup/ItemsSetup.java
@@ -2,14 +2,20 @@ package io.github.thehrz.snowcraft.setup;
 
 import io.github.thehrz.snowcraft.list.Items;
 import io.github.thehrz.snowcraft.object.machines.*;
+import io.izzel.taboolib.module.light.TLight;
+import io.izzel.taboolib.module.nms.impl.Type;
 import me.mrCookieSlime.CSCoreLibPlugin.general.Inventory.Item.CustomItem;
 import me.mrCookieSlime.Slimefun.Lists.NarItems;
 import me.mrCookieSlime.Slimefun.Lists.RecipeType;
 import me.mrCookieSlime.Slimefun.Lists.SlimefunItems;
 import me.mrCookieSlime.Slimefun.Objects.Research;
+import me.mrCookieSlime.Slimefun.Objects.SlimefunBlockHandler;
 import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
+import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.UnregisterReason;
 import me.mrCookieSlime.Slimefun.api.Slimefun;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 /**
@@ -62,18 +68,18 @@ public class ItemsSetup {
                 return 512;
             }
         }.registerChargeableBlock(false, 2048);
-//        new SlimefunItem(CategoriesSetup.SnowCraft_Misc, Items.LANTERN, "LANTERN", RecipeType.ENHANCED_CRAFTING_TABLE, new ItemStack[]{new ItemStack(Material.IRON_INGOT), new ItemStack(Material.TRIPWIRE_HOOK), new ItemStack(Material.IRON_INGOT), new ItemStack(Material.IRON_INGOT), new ItemStack(Material.TORCH), new ItemStack(Material.IRON_INGOT), new ItemStack(Material.IRON_INGOT), new ItemStack(Material.IRON_INGOT), new ItemStack(Material.IRON_INGOT)}, new CustomItem(Items.LANTERN, 6)).register(false, new SlimefunBlockHandler() {
-//            @Override
-//            public void onPlace(Player player, Block block, SlimefunItem slimefunItem) {
-//                TLight.create(block, Type.BLOCK, 15);
-//            }
-//
-//            @Override
-//            public boolean onBreak(Player player, Block block, SlimefunItem slimefunItem, UnregisterReason unregisterReason) {
-//                TLight.delete(block.getLocation(), Type.BLOCK);
-//                return true;
-//            }
-//        });
+        new SlimefunItem(CategoriesSetup.SnowCraft_Misc, Items.LANTERN, "LANTERN", RecipeType.ENHANCED_CRAFTING_TABLE, new ItemStack[]{new ItemStack(Material.IRON_INGOT), new ItemStack(Material.TRIPWIRE_HOOK), new ItemStack(Material.IRON_INGOT), new ItemStack(Material.IRON_INGOT), new ItemStack(Material.TORCH), new ItemStack(Material.IRON_INGOT), new ItemStack(Material.IRON_INGOT), new ItemStack(Material.IRON_INGOT), new ItemStack(Material.IRON_INGOT)}, new CustomItem(Items.LANTERN, 6)).register(false, new SlimefunBlockHandler() {
+            @Override
+            public void onPlace(Player player, Block block, SlimefunItem slimefunItem) {
+                TLight.create(block.getLocation(), Type.BLOCK, 15);
+            }
+
+            @Override
+            public boolean onBreak(Player player, Block block, SlimefunItem slimefunItem, UnregisterReason unregisterReason) {
+                TLight.delete(block, Type.BLOCK);
+                return true;
+            }
+        });
 
         new AbstractBlockCompresMachine(CategoriesSetup.SnowCraft_Technology, Items.BLOCK_COMPRES_MACHINE, "BLOCK_COMPRES_MACHINE", RecipeType.ENHANCED_CRAFTING_TABLE, new ItemStack[]{SlimefunItems.ELECTRIC_MOTOR, new ItemStack(Material.PISTON_BASE), SlimefunItems.ELECTRIC_MOTOR, SlimefunItems.ALUMINUM_BRASS_INGOT, SlimefunItems.HARDENED_METAL_INGOT, SlimefunItems.ALUMINUM_BRASS_INGOT, null, new ItemStack(Material.OBSIDIAN), null}) {
             @Override
@@ -121,7 +127,7 @@ public class ItemsSetup {
         new SlimefunItem(CategoriesSetup.SnowCraft_Misc, Items.SNOW_GOLEM_STICK, "SNOW_GOLEM_STICK", RecipeType.ENHANCED_CRAFTING_TABLE, new ItemStack[]{SlimefunItems.MAGIC_LUMP_3, SlimefunItems.BATTERY, SlimefunItems.MAGIC_LUMP_3, new ItemStack(Material.SNOW_BLOCK), SlimefunItems.STAFF_WATER, new ItemStack(Material.SNOW_BLOCK), null, SlimefunItems.STAFF_ELEMENTAL, null}).register(false);
 
         Slimefun.registerResearch(new Research(3000, "§bBLOCK_COMPRES_MACHINE", 32), Items.BLOCK_COMPRES_MACHINE, Items.BLOCK_COMPRES_MACHINE_2, Items.BLOCK_COMPRES_MACHINE_3);
-//        Slimefun.registerResearch(new Research(3001, "§bLANTERN", 16), Items.LANTERN);
+        Slimefun.registerResearch(new Research(3001, "§bLANTERN", 16), Items.LANTERN);
         Slimefun.registerResearch(new Research(3002, "§bANDROID_PANEL", 128), Items.ANDROID_PANEL);
         Slimefun.registerResearch(new Research(3003, "§aIRON_GOLEM_PROGRAM", 16), Items.IRON_GOLEM_ASSEMBLER);
         Slimefun.registerResearch(new Research(3004, "§cMAGNESIUM_GENERATOR", 32), Items.MAGNESIUM_GENERATOR);
